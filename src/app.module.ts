a0a769801abc06c34bae0bbfc82c4f913323282f
Ha Phi Hung <haphihung55@gmail.com>
2023-03-30 01:02:23 +0700

    Setup components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
+import { EmployeesModule } from './employees/employees.module';
+import { CartsController } from './customers/carts.controller';
+import { CartsModule } from './carts/carts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ProductsModule,
+    OrdersModule,
+    UsersModule,
+    CustomersModule,
+    EmployeesModule,
+    CartsModule,
+  ],
+  controllers: [AppController, CartsController],
   providers: [AppService],
 })
 export class AppModule {}
