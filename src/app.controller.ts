33b3662038057937b1d99476235bd89c0f023032
Ha Phi Hung <haphihung55@gmail.com>
2023-04-02 00:53:08 +0700

    Setup basic view files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
+  @Render('homepage')
   getIndex() {
     return { text: this.appService.getHello(), title: 'Index' };
   }
