b9d300d9eeee63db0db27fd6b57c734d063ee331
Ha Phi Hung <haphihung55@gmail.com>
2023-04-01 21:03:02 +0700

    Fix products endpoint tests lack prisma provider

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { PrismaService } from 'nestjs-prisma';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [ProductsService, PrismaService],
       controllers: [ProductsController],
     }).compile();
 
