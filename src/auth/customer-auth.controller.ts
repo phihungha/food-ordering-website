1e343ac2fd8b21d144fdbed78ca273644fad7833
Ha Phi Hung <haphihung55@gmail.com>
2023-04-09 01:55:19 +0700

    Implement logout

--- src/auth/customer-auth.controller.ts
+++ src/auth/customer-auth.controller.ts
@@ -14,21 +14,27 @@ import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { User } from '@prisma/client';
 
-@Controller('login')
+@Controller()
 export class CustomerAuthController {
   constructor(private authService: AuthService) {}
 
-  @Get()
+  @Get('login')
   @Render('login')
-  async getLogin(@Query('signupSucceed') signupSucceed: boolean) {
+  async getLoginPage(@Query('signupSucceed') signupSucceed: boolean) {
     return { title: 'Đăng nhập ABC', signupSucceed };
   }
 
   @UseGuards(LocalAuthGuard)
-  @Post()
+  @Post('login')
   @Redirect('/')
   async login(@Req() req: Request, @Res() res: Response) {
     const jwt = await this.authService.generateJwt(req.user as User);
-    res.cookie('jwt', jwt);
+    res.cookie('jwt', jwt, { httpOnly: true });
+  }
+
+  @Get('logout')
+  @Redirect('/')
+  async logout(@Res() res: Response) {
+    res.cookie('jwt', '', { maxAge: 1 });
   }
 }
