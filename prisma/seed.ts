55f93de9d618f2faf4b037c3b60e7aebed9d3374
Ha Phi Hung <haphihung55@gmail.com>
2023-04-09 16:34:48 +0700

    Improve DB seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import {
   EmployeeType,
   OrderStatus,
@@ -7,16 +8,33 @@ import {
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const customer = await prisma.customer.create({
+async function seedDb() {
+  const salt = await bcrypt.genSalt();
+  const password = await bcrypt.hash('12345678', salt);
+
+  const customer1 = await prisma.customer.create({
     data: {
       user: {
         create: {
           name: 'Nguyễn Văn A',
           email: 'nguyenvana@gmail.com',
-          phoneNumber: '0123456789',
+          phoneNumber: '0123456780',
           type: UserType.Customer,
-          hashedPassword: '1234',
+          hashedPassword: password,
+        },
+      },
+    },
+  });
+
+  const customer2 = await prisma.customer.create({
+    data: {
+      user: {
+        create: {
+          name: 'Nguyễn Văn B',
+          email: 'nguyenvanb@gmail.com',
+          phoneNumber: '0123456781',
+          type: UserType.Customer,
+          hashedPassword: password,
         },
       },
     },
@@ -28,12 +46,27 @@ async function main() {
         create: {
           name: 'Nguyễn Đơn Hàng',
           email: 'nguyendonhang@gmail.com',
-          phoneNumber: '012342322',
+          phoneNumber: '0123456782',
           type: UserType.Employee,
-          hashedPassword: '1234',
+          hashedPassword: password,
         },
       },
       type: EmployeeType.OrderManager,
+    },
+  });
+
+  await prisma.employee.create({
+    data: {
+      user: {
+        create: {
+          name: 'Nguyễn Hàng Hóa',
+          email: 'nguyenhanghoa@gmail.com',
+          phoneNumber: '0123456783',
+          type: UserType.Employee,
+          hashedPassword: password,
+        },
+      },
+      type: EmployeeType.InventoryManager,
     },
   });
 
@@ -43,7 +76,10 @@ async function main() {
         name: 'Snack',
       },
       {
-        name: 'Dairy',
+        name: 'Sữa',
+      },
+      {
+        name: 'Rau củ',
       },
     ],
   });
@@ -54,36 +90,47 @@ async function main() {
       category: {
         connect: { name: 'Snack' },
       },
-      unit: 'item(s)',
+      unit: 'cái',
       price: 10000,
     },
   });
 
   await prisma.product.create({
     data: {
-      name: 'Bánh bắp',
+      name: 'Bánh Bắp Ngọt',
       category: {
         connect: { name: 'Snack' },
       },
-      unit: 'item(s)',
+      unit: 'cái',
       price: 5000,
     },
   });
 
   await prisma.product.create({
     data: {
-      name: 'Milo Small',
+      name: 'Milo',
       category: {
-        connect: { name: 'Dairy' },
+        connect: { name: 'Sữa' },
       },
-      unit: 'item(s)',
+      unit: 'cái',
+      price: 8000,
+    },
+  });
+
+  await prisma.product.create({
+    data: {
+      name: 'Rau muống',
+      category: {
+        connect: { name: 'Rau củ' },
+      },
+      unit: 'kg',
       price: 8000,
     },
   });
 
   await prisma.order.create({
     data: {
-      customer: { connect: { id: customer.id } },
+      customer: { connect: { id: customer1.id } },
       items: {
         create: [
           {
@@ -93,13 +140,13 @@ async function main() {
             itemPrice: 10000,
           },
           {
-            product: { connect: { name: 'Milo Small' } },
+            product: { connect: { name: 'Milo' } },
             quantity: 1,
             unitPrice: 8000,
             itemPrice: 8000,
           },
           {
-            product: { connect: { name: 'Bánh bắp' } },
+            product: { connect: { name: 'Bánh Bắp Ngọt' } },
             quantity: 2,
             unitPrice: 5000,
             itemPrice: 10000,
@@ -113,11 +160,11 @@ async function main() {
 
   await prisma.order.create({
     data: {
-      customer: { connect: { id: customer.id } },
+      customer: { connect: { id: customer1.id } },
       items: {
         create: [
           {
-            product: { connect: { name: 'Milo Small' } },
+            product: { connect: { name: 'Milo' } },
             quantity: 2,
             unitPrice: 8000,
             itemPrice: 16000,
@@ -130,9 +177,27 @@ async function main() {
     },
   });
 
+  await prisma.order.create({
+    data: {
+      customer: { connect: { id: customer2.id } },
+      items: {
+        create: [
+          {
+            product: { connect: { name: 'Rau muống' } },
+            quantity: 2,
+            unitPrice: 8000,
+            itemPrice: 16000,
+          },
+        ],
+      },
+      totalAmount: 16000,
+      status: OrderStatus.Pending,
+    },
+  });
+
   await prisma.cartItem.create({
     data: {
-      customer: { connect: { id: customer.id } },
+      customer: { connect: { id: customer1.id } },
       product: { connect: { name: 'Lays Potato' } },
       quantity: 1,
     },
@@ -140,14 +205,22 @@ async function main() {
 
   await prisma.cartItem.create({
     data: {
-      customer: { connect: { id: customer.id } },
-      product: { connect: { name: 'Milo Small' } },
+      customer: { connect: { id: customer1.id } },
+      product: { connect: { name: 'Rau muống' } },
+      quantity: 1,
+    },
+  });
+
+  await prisma.cartItem.create({
+    data: {
+      customer: { connect: { id: customer2.id } },
+      product: { connect: { name: 'Milo' } },
       quantity: 2,
     },
   });
 }
 
-main()
+seedDb()
   .then(async () => {
     await prisma.$disconnect();
   })
