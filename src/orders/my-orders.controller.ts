8a0b125edf2d034ab2583bbdb4561619fd5d14f9
Ha Phi Hung <haphihung55@gmail.com>
2023-04-10 00:57:16 +0700

    Redirect to order details page after placing order

--- src/orders/my-orders.controller.ts
+++ src/orders/my-orders.controller.ts
@@ -9,9 +9,11 @@ import {
   Redirect,
   Render,
   Req,
+  Res,
+  UseInterceptors,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { User } from '@prisma/client';
 import { PlaceOrderDto } from './place-order.dto';
 
@@ -39,13 +41,17 @@ export class MyOrdersController {
   }
 
   @Post()
-  async placeOrder(@Body() body: PlaceOrderDto, @Req() req: Request) {
+  async placeOrder(
+    @Body() body: PlaceOrderDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
     const currentUser = req.user! as User;
     const order = await this.ordersService.placeOrder(
       body.deliveryAddress,
       currentUser.id,
     );
-    return { url: `order-details/${order.id}` };
+    res.redirect(`my-orders/${order.id}`);
   }
 
   @Delete(':id')
