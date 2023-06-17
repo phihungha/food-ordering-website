8d009e23a5ae087db485e42727289e7bb0b55d96
Ha Phi Hung <haphihung55@gmail.com>
2023-06-17 23:37:59 +0700

    Add admin product controller to module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { MyCartModule } from 'src/carts/my-cart.module';
+import { AdminProductsController } from './admin-products.controller';
 
 @Module({
   imports: [MyCartModule],
   providers: [ProductsService],
-  controllers: [ProductsController],
+  controllers: [ProductsController, AdminProductsController],
   exports: [ProductsService],
 })
 export class ProductsModule {}
