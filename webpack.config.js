056a1fe57224742e619978397c266879f1963528
Ha Phi Hung <haphihung55@gmail.com>
2023-06-21 00:40:48 +0700

    Install firebase auth ui and setup css webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
 const path = require('path');
 const glob = require('glob');
 
-const client = {
-  entry: glob.sync('./src/public/scripts/**.ts').reduce((result, item) => {
-    result[path.parse(item).name] = item;
-    return result;
-  }, {}),
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'dist/public/scripts'),
-    filename: '[name].js',
-  },
-  optimization: {
-    splitChunks: { chunks: 'all' },
-  },
-  plugins: [],
-  // Disable nest build default ignore options
-  externalsPresets: {},
-  externals: [],
-};
+function client(options) {
+  return {
+    entry: glob.sync('./src/public/scripts/**.ts').reduce((result, item) => {
+      result[path.parse(item).name] = item;
+      return result;
+    }, {}),
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, 'dist/public/scripts'),
+      filename: '[name].js',
+    },
+    module: {
+      rules: [
+        ...options.module.rules,
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      ],
+    },
+    optimization: {
+      splitChunks: { chunks: 'all' },
+    },
+    plugins: [],
+    // Disable nest build default ignore options
+    externalsPresets: {},
+    externals: [],
+  };
+}
 
 function server(options) {
   return options;
